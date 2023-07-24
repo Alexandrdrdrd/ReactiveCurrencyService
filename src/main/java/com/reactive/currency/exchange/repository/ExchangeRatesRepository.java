0902b1e91d0126833a8c081f23832da38ea09aad
Alexandrdrdrd <kands12349@gmail.com>
2023-07-24 12:56:32 +0300

    fixed repository query

--- src/main/java/com/reactive/currency/exchange/repository/ExchangeRatesRepository.java
+++ src/main/java/com/reactive/currency/exchange/repository/ExchangeRatesRepository.java
@@ -1,13 +1,19 @@
 package com.reactive.currency.exchange.repository;
 
 import com.reactive.currency.exchange.entity.ExchangeRate;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 
 @Repository
 public interface ExchangeRatesRepository extends ReactiveCrudRepository<ExchangeRate, Integer> {
 
+    @Query("SELECT * FROM exchange_rates ORDER BY id DESC LIMIT 61")
+    Flux<ExchangeRate> findAll();
+
+    @Query("SELECT * FROM exchange_rates WHERE cc = :cc ORDER BY id DESC LIMIT 1")
     Mono<ExchangeRate> findDistinctFirstByCc(String cc);
 }
